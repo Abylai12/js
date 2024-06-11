9f9200e932008bf25015945ee4b7d2e66ce1ae94
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-06-12 01:06:48 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,172 @@
+// bodlogo 1
+let myAge = 16;
+if (myAge >= 25) {
+  console.log("above 25");
+} else {
+  console.log("below 25");
+}
+//bodlogo 2
+// let n = Number(prompt("too oruulna uu"));
+// if (!(n % 2)) {
+//   console.log("it's even number");
+// } else {
+//   console.log("it's odd number");
+// }
+// bodlogo 3
+// let a = 12,
+//   b = 16,
+//   c = 1;
+// if (a < b) {
+//   if (c < b) {
+//     console.log("hamgiin ih n: ", b);
+//   } else {
+//     console.log("hamgiin ih n: ", c);
+//   }
+// } else if (a < c) {
+//   console.log("hamgiin ih n: ", c);
+// } else {
+//   console.log("hamgiin ih n: ", a);
+// }
+// bodlogo4
+// if (a > b) {
+//   if (c > b) {
+//     console.log("hamgiin baga n: ", b);
+//   } else {
+//     console.log("hamgiin baga n: ", c);
+//   }
+// } else if (a > c) {
+//   console.log("hamgiin baga n: ", c);
+// } else {
+//   console.log("hamgiin baga n: ", a);
+// }
+
+//bodlogo 5
+// let month = Number(prompt("too oruulna uu"));
+// if (2 <= month && month <= 4) {
+//   console.log("Spring");
+// } else if (5 <= month && month <= 7) {
+//   console.log("Summer");
+// } else if (8 <= month && month <= 10) {
+//   console.log("Autumn");
+// } else if (11 == month && month == 12) {
+//   console.log("Winter");
+// } else if (month == 1) {
+//   console.log("Winter");
+// } else {
+//   console.log("utga oldsongui");
+// }
+// bodlogo 6
+// let x = 85,
+//   y = 75,
+//   z = 96,
+//   q = 69,
+//   sum = 0;
+// if (80 <= x) {
+//   sum += x;
+// }
+// if (80 <= y) {
+//   sum += y;
+// }
+// if (80 <= z) {
+//   sum += z;
+// }
+// if (80 <= q) {
+//   sum += q;
+// }
+// console.log(sum);
+// bodlogo 7
+// let x = 3,
+//   y = 7,
+//   z = 2,
+//   q = 4,
+//   multiply = 1;
+// if (5 >= x) {
+//   multiply *= x;
+// }
+// if (5 >= y) {
+//   multiply *= y;
+// }
+// if (5 >= z) {
+//   multiply *= z;
+// }
+// if (5 >= q) {
+//   multiply *= q;
+// }
+// console.log(multiply);
+//bodlogo9
+let a = 12,
+  b = 33,
+  c = 10,
+  d = 12;
+if (a < b && a < c && a < d) {
+  console.log("hamgiin baga ni: ", a);
+} else if (b < a && b < c && b < d) {
+  console.log("hamgiin baga ni: ", b);
+} else if (c < a && c < d && c < b) {
+  console.log("hamgiin baga ni: ", c);
+} else {
+  console.log("hamgiin baga ni: ", d);
+}
+// bodlogo 8
+// let bat = 90,
+//   ochir = 59,
+//   onoo = bat;
+// if (onoo < 60) {
+//   console.log("mash muu");
+// } else if (60 <= onoo && onoo < 70) {
+//   console.log("hangalttai");
+// } else if (70 <= onoo && onoo < 80) {
+//   console.log("Dund");
+// } else if (80 <= onoo && onoo < 90) {
+//   console.log("Sain");
+// } else if (90 <= onoo && onoo < 100) {
+//   console.log("Mash sain");
+// } else {
+//   console.log("utga oldsongui");
+// }
+let bat = 90,
+  ochir = 59,
+  onoo = bat;
+if (onoo < 60) {
+  console.log("mash muu");
+} else if (60 <= onoo && onoo < 70) {
+  console.log("hangalttai");
+} else if (70 <= onoo && onoo < 80) {
+  console.log("Dund");
+} else if (80 <= onoo && onoo < 90) {
+  console.log("Sain");
+} else if (90 <= onoo && onoo < 100) {
+  console.log("Mash sain");
+} else {
+  console.log("utga oldsongui");
+}
+// bodlogo 11
+// let height = Number(prompt("unduruu oruulna uu"));
+// let age = Number(prompt("nasaa oruulna uu"));
+// // let hasAdult = Number(prompt("unduruu oruulna uu"));
+// if (height >= 140 && age >= 14) {
+//   console.log("allowed to ride");
+// } else {
+//   console.log("not allowed to ride");
+// }
+//bodlogo 12
+const x = 7,
+  y = 22,
+  z = 13,
+  v = 30;
+let sum = 0;
+// const x=[7, 22, 13, 30]
+// for ()
+if (x % 11) {
+  sum += x;
+}
+if (y % 11) {
+  sum += y;
+}
+if (z % 11) {
+  sum += z;
+}
+if (v % 11) {
+  sum += v;
+}
+console.log(sum);
